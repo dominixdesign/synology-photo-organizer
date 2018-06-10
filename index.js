2a570ea9e801d81db0f40c070a68c78ce7fd6574
dsa <dominik.sander@googlemail.com>
2018-06-10 21:19:30 +0200

    move config to config.json

--- index.js
+++ index.js
@@ -6,21 +6,15 @@ const { DateTime } = require('luxon');
 const { exec } = require('child_process');
 const ExifTool = require('exiftool-kit')
 const exiftool = new ExifTool()
+const config = require('./config.json')
+
+console.log(config)
 
 const log = (photo, message) => {
 	if(true) console.log(photo, '  #####  ' ,message)
 };
 
-//FOLDERS:
-
-const importFolder = '/volume2/photo/__import/'
-const errorFolder = '/volume2/photo/__import-error/'
-const invalidFolder = '/volume2/photo/__import-no-photos/'
-const outputFolder = '/volume2/photo/'
-
-
-
-var watcher = chokidar.watch(importFolder, {
+var watcher = chokidar.watch(config.importFolder, {
   ignored: /(^|[\/\\])\../,
   persistent: true,
   awaitWriteFinish: {
@@ -33,7 +27,7 @@ watcher.on('add', filepath => {
 	if(filepath.indexOf('SynoEAStream')>=0 || filepath.indexOf('@eaDir')>=0) {
 		return false;
 	}
-	
+
 	if(path.extname(filepath).toLowerCase() == '.jpg') {
 		log(filepath,'its a photo');
 		exif.read(filepath).then(data => {
@@ -50,7 +44,7 @@ watcher.on('add', filepath => {
 					]
 					times.sort();
 					copyFileByDate(filepath, new Date(times.shift()));
-					
+
 				})
 			} else {
 				copyFileByDate(filepath, datetime);
@@ -61,7 +55,7 @@ watcher.on('add', filepath => {
 	} else {
 		//no image, move to different folder
 		log(filepath,'not a photo, move to no-photo folder.');
-		fs.rename(filepath, invalidFolder+path.basename(filepath), (err) => {
+		fs.rename(filepath, config.invalidFolder+path.basename(filepath), (err) => {
 		  if (err) throw err;
 		})
 	}
@@ -69,11 +63,11 @@ watcher.on('add', filepath => {
 
 var copyFileByDate = (filepath, datetime) => {
 	log(filepath,'start copy');
-	var dir = outputFolder + datetime.getFullYear() + '/' + DateTime.fromJSDate(datetime).toISODate()
-	if(!fs.existsSync(outputFolder + datetime.getFullYear())) {
+	var dir = config.outputFolder + datetime.getFullYear() + '/' + DateTime.fromJSDate(datetime).toISODate()
+	if(!fs.existsSync(config.outputFolder + datetime.getFullYear())) {
 		log(filepath,'create year folder: "' + datetime.getFullYear() + '"');
-		fs.mkdirSync(outputFolder + datetime.getFullYear());
-		exec('synoindex -A '+outputFolder + datetime.getFullYear(), (err, stdout, stderr) => {
+		fs.mkdirSync(config.outputFolder + datetime.getFullYear());
+		exec('synoindex -A '+config.outputFolder + datetime.getFullYear(), (err, stdout, stderr) => {
 			if (err) throw err;
 			log(filepath,'index updated');
 		});
@@ -89,8 +83,8 @@ var copyFileByDate = (filepath, datetime) => {
 	fs.rename(filepath, dir+'/'+path.basename(filepath), (err) => {
 		if (err) throw err;
 		log(filepath,'sorted photo "'+path.basename(filepath)+'" successfully.');
-	
-		var tags = filepath.replace(importFolder,'').replace(path.basename(filepath),'').replace('/','')
+
+		var tags = filepath.replace(config.importFolder,'').replace(path.basename(filepath),'').replace('/','')
 		if(tags) {
 			log(filepath,'write tags: ' + tags);
 			exiftool.setTags({
@@ -100,7 +94,7 @@ var copyFileByDate = (filepath, datetime) => {
 				]
 			})
 		}
-		
+
 		exec('synoindex -a '+dir+'/'+path.basename(filepath), (err, stdout, stderr) => {
 		  if (err) throw err;
 		  log(filepath,'index updated');
